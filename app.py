2ea71223b08f884a0a35007f5052f0a0c1851ef4
harshVardhanLab <harshvardhan02102002@gmail.com>
2026-03-13 08:28:38 +0530

    30s

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     ping_thread.start()
 
     # Get port from environment variable
-    port = int(os.environ.get('PORT', 4000))
+    port = int(os.environ.get('PORT', 5000))
 
     # Run Flask app
     app.run(host='0.0.0.0', port=port, debug=False)
